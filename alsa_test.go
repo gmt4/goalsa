a143180b5d218111e307b77105cc4d9027074eb6
gmt <791491+gmt4@users.noreply.github.com>
2022-03-31 21:12:32 +0200

    Update alsa_test.go

--- alsa_test.go
+++ alsa_test.go
@@ -28,8 +28,14 @@ func TestCapture(t *testing.T) {
 
 	a.NoError(err, "created capture device")
 
+	b0 := 3
+	samples, err := c.Read(b0)
+
+	a.Error(err, "wrong type error")
+	a.Equal(samples, 0, "no samples read")
+
 	b1 := make([]int8, 100)
-	samples, err := c.Read(b1)
+	samples, err = c.Read(b1)
 
 	a.Error(err, "wrong type error")
 	a.Equal(samples, 0, "no samples read")
@@ -53,6 +59,45 @@ func TestCapture(t *testing.T) {
 	a.Equal(len(b2), samples, "correct number of samples read")
 
 	c.Close()
+
+	// extras ints
+
+	c, err = NewCaptureDevice("null", 1, FormatS8, 22050, BufferParams{})
+	a.NoError(err, "created capture device")
+
+	samples, err = c.Read(b1)
+
+	a.NoError(err, "read samples ok")
+	a.Equal(len(b1), samples, "correct number of samples read")
+	c.Close()
+
+	c, err = NewCaptureDevice("null", 1, FormatS16LE, 22050, BufferParams{})
+	a.NoError(err, "created capture device")
+
+	samples, err = c.Read(b2)
+
+	a.NoError(err, "read samples ok")
+	a.Equal(len(b2), samples, "correct number of samples read")
+	c.Close()
+
+	samples, err = c.Read(b4)
+
+	a.Error(err, "wrong type error")
+	a.Equal(samples, 0, "no samples read")
+	c.Close()
+
+	// extras floats
+
+	c, err = NewCaptureDevice("null", 1, FormatFloat64LE, 22050, BufferParams{})
+	a.NoError(err, "created capture device")
+
+	b5 := make([]float64, 200)
+	samples, err = c.Read(b5)
+
+	a.NoError(err, "read samples ok")
+	a.Equal(len(b5), samples, "correct number of samples read")
+	c.Close()
+
 }
 
 func TestPlayback(t *testing.T) {
@@ -97,7 +142,67 @@ func TestPlayback(t *testing.T) {
 	frames, err = p.Write(b4)
 
 	a.NoError(err, "buffer written ok")
-	a.Equal(frames, 100, "100 frames written")
+	a.Equal(frames, len(b4), "correct frames written")
 
 	p.Close()
+
+	// extras  s16
+
+	p, err = NewPlaybackDevice("null", 1, FormatS16LE, 44100, BufferParams{})
+
+	a.NoError(err, "created playback device")
+
+	frames, err = p.Write(b4)
+
+	a.Error(err, "wrong type error")
+	a.Equal(frames, 0, "no frames written")
+
+	frames, err = p.Write(b2)
+
+	a.NoError(err, "buffer written ok")
+	a.Equal(frames, len(b2), "correct frames written")
+
+	p.Close()
+}
+
+/*
+func TestPanics(t *testing.T) {
+	//a := assert.New(t)
+
+	// Capture with Format Unknown must panic()
+	shouldPanic(t, func() {
+		NewCaptureDevice("null", 1, Format(-1), 44100, BufferParams{})
+	})
+
+	// Capture reading with Unknown format must panic()
+	shouldPanic(t, func() {
+		c, err0 := NewCaptureDevice("null", 1, FormatS8, 44100, BufferParams{})
+		a.NoError(err0, "created playback device")
+
+		c.Format = Format(-1)
+		b1 := make([]int8, 100)
+		c.Read(b1)
+	})
+
+	// Playback with Format Unknown must panic()
+	shouldPanic(t, func() {
+		NewPlaybackDevice("null", 1, Format(-1), 44100, BufferParams{})
+	})
+
+	// Plaback writing with Unknown format must panic()
+	shouldPanic(t, func() {
+		p, err0 := NewPlaybackDevice("null", 1, FormatS8, 44100, BufferParams{})
+		a.NoError(err0, "created playback device")
+
+		p.Format = Format(-1)
+		b1 := make([]int8, 100)
+		p.Write(b1)
+	})
+}
+*/
+
+func shouldPanic(t *testing.T, f func()) {
+    defer func() { recover() }()
+    f()
+    t.Errorf("should have panicked")
 }
